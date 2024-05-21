66d318429ef124ca87050b1f8bfcf710a857be36
Sören Langenberg <76487343+soelangen@users.noreply.github.com>
2024-05-21 16:39:31 +0200

    Add: Workflow for debian packages (#6)
    - Create Server package
    - Create Client package
    - Automatic deployment

--- .github/build.sh
+++ .github/build.sh
@@ -7,7 +7,7 @@ export INSTALL_DIR=${DEFAULT_INSTALL_DIR}
 export OPENSSH_BUILD_OPTIONS=""
 cd /opt
 git clone --depth 1 --branch main https://github.com/open-quantum-safe/liboqs
-git clone --depth 1 --branch OQS-v8 https://github.com/open-quantum-safe/openssh ossh-src
+git clone --depth 1 --branch OQS-v8 https://github.com/enclaive/buckypaper-openssh ossh-src
 cd /opt/liboqs
 mkdir build-static && cd build-static && cmake .. ${LIBOQS_BUILD_DEFINES} -DCMAKE_BUILD_TYPE=${LIBOQS_BUILD_TYPE} -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=/opt/ossh-src/oqs && make ${MAKE_DEFINES} && make install
 cd /opt/ossh-src
---
+++ .github/build_debian_pkg.sh
@@ -0,0 +1,46 @@
+#/bin/sh
+set -ex
+
+## Prerequisites
+mkdir /assets/
+ls /opt/ossh-src/debian/openssh-pq-server/etc
+
+##Building the Client
+mkdir -p /opt/ossh-src/debian/openssh-pq-client/etc/ssh/ssh_config.d
+mv /opt/ossh-src/ssh_config /opt/ossh-src/debian/openssh-pq-client/etc/ssh/ssh_config
+
+mv /opt/ossh-src/scp /opt/ossh-src/debian/openssh-pq-client/usr/bin/scp
+mv /opt/ossh-src/sftp /opt/ossh-src/debian/openssh-pq-client/usr/bin/sftp
+
+mv /opt/ossh-src/ssh /opt/ossh-src/debian/openssh-pq-client/usr/bin/ssh
+mv /opt/ossh-src/ssh-add /opt/ossh-src/debian/openssh-pq-client/usr/bin/ssh-add 
+mv /opt/ossh-src/ssh-agent /opt/ossh-src/debian/openssh-pq-client/usr/bin/ssh-agent
+mv /opt/ossh-src/ssh-keygen /opt/ossh-src/debian/openssh-pq-client/usr/bin/ssh-keygen
+mv /opt/ossh-src/ssh-keyscan /opt/ossh-src/debian/openssh-pq-client/usr/bin/ssh-keyscan
+
+mv /opt/ossh-src/ssh-keysign /opt/ossh-src/debian/openssh-pq-client/usr/lib/openssh/ssh-keysign 
+mv /opt/ossh-src/ssh-pkcs11-helper /opt/ossh-src/debian/openssh-pq-client/usr/lib/openssh/ssh-pkcs11-helper
+mv /opt/ossh-src/ssh-sk-helper /opt/ossh-src/debian/openssh-pq-client/usr/lib/openssh/ssh-sk-helper
+
+mkdir /opt/ossh-src/debian/openssh-pq-client/usr/lib/systemd/user/graphical-session-pre.target.wants
+ln -sf /opt/ossh-src/debian/openssh-pq-client/usr/lib/systemd/user/ssh-agent.service /opt/ossh-src/debian/openssh-pq-client/usr/lib/systemd/user/graphical-session-pre.target.wants
+ln -sf /opt/ossh-src/debian/openssh-pq-client/usr/bin/ssh /opt/ossh-src/debian/openssh-pq-client/usr/bin/slogin
+
+cd /opt/ossh-src/debian/ && dpkg-deb --build openssh-pq-client
+mv /opt/ossh-src/debian/openssh-pq-client.deb /assets/
+
+
+## Building the Server
+mkdir -p /opt/ossh-src/debian/openssh-pq-server/etc/runit/runsvdir/default
+mkdir -p /opt/ossh-src/debian/openssh-pq-server/etc/ssh/sshd_config.d
+mkdir -p /opt/ossh-src/debian/openssh-pq-server/sbin
+mkdir -p /opt/ossh-src/debian/openssh-pq-server/var/log/runit/ssh
+mkdir -p /opt/ossh-src/debian/openssh-pq-server/usr/share/openssh
+
+mv /opt/ossh-src/sshd_config /opt/ossh-src/debian/openssh-pq-server/usr/share/openssh
+
+mv /opt/ossh-src/sshd /opt/ossh-src/debian/openssh-pq-server/sbin/sshd
+ln -sf /opt/ossh-src/debian/openssh-pq-server/usr/share/doc/openssh-client /opt/ossh-src/debian/openssh-pq-server/usr/share/doc/openssh-server
+
+cd /opt/ossh-src/debian/ && dpkg-deb --build openssh-pq-server
+mv /opt/ossh-src/debian/openssh-pq-server.deb /assets/
